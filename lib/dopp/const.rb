77dea4908d160478650d9ba0ffe70e34b72a6b36
srtkkou <srtkkou@outlook.jp>
2020-01-24 15:36:05 +0900

    [Add] Added const.rb file to define constants. Moved VERSION inside it.
    * 定数を定義するconst.rbを作成。
    * version.rbを削除し、VERSION定数はconst.rbに移動。

---
+++ lib/dopp/const.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# File to define module constants.
+# 定数定義用ファイル.
+module Dopp
+  # Version of the application.
+  # アプリケーションのバージョン.
+  VERSION ||= '0.1.0'.freeze
+
+  # Application name.
+  # アプリケーション名.
+  APPLICATION ||= self.name.freeze
+
+  # Line feed code.
+  # 改行コード.
+  LF ||= "\n".freeze
+end
+
