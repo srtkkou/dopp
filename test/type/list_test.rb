74a67655d680f62bae9e6495b971c37e8f2cf818
srtkkou <srtkkou@outlook.jp>
2020-01-29 11:16:59 +0900

    [Fix] Fixed List testing class assertions.
    * Fixed the order of arguments in ListTest.
     (配列試験のassertの引数の順番が間違っていたのを修正).

--- test/type/list_test.rb
+++ test/type/list_test.rb
@@ -6,26 +6,28 @@ module Dopp
   module Type
     class ListTest < Minitest::Test
       def test_OK_delegated
-        list = List.new(0, 1, 2, '3', '4')
+        # empty?
+        assert(List.new.empty?)
         # length, size
+        list = List.new(0, 1, 2, '3', '4')
         assert_equal(5, list.size)
         assert_equal(5, list.length)
         # [], at
-        assert_equal(list[0], 0)
-        assert_equal(list.at(1), 1)
-        assert_equal(list[2], 2)
-        assert_equal(list.at(3), '3')
-        assert_equal(list[-1], '4')
+        assert_equal(0, list[0])
+        assert_equal(1, list.at(1))
+        assert_equal(2, list[2])
+        assert_equal('3', list.at(3))
+        assert_equal('4', list[-1])
         # []=
         list[2] = 'NEW2'
-        assert_equal(list[2], 'NEW2')
+        assert_equal('NEW2', list[2])
         # <<
         list << 5
-        assert_equal(list[-1], 5)
+        assert_equal(5, list[-1])
       end
 
       def test_OK_to_s
-        assert_equal('[ ]', List.new().to_s)
+        assert_equal('[ ]', List.new.to_s)
         assert_equal('[1 2 3 4]', List.new(1, 2, 3, 4).to_s)
       end
     end
