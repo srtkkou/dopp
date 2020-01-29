5ac86bae6c01660a62a34d8c54365d1fa622c391
srtkkou <srtkkou@outlook.jp>
2020-01-29 13:20:36 +0900

    [BugFix] Fixed Util#deep_freeze.
    * When hash is applied to old deep_freeze, it was converted to array because
     of the variable length argument. Changed it to a single argument.
     (deep_freezeにハッシュを渡したときに可変長引数だと勝手に配列に変更されるので
     単一のオブジェクトを処理するよう修正した。).

--- lib/dopp/util.rb
+++ lib/dopp/util.rb
@@ -5,18 +5,19 @@ module Dopp
     module_function
 
     # Deep freeze.
-    def deep_freeze(*args)
-      args.each do |arg|
-        case arg
-        when Hash
-          arg.each{|k, v| deep_freeze(k, v)}
-          arg.freeze
-        when Enumerable
-          arg.each{|v| deep_freeze(v)}
-          arg.freeze
-        else
-          arg.freeze unless arg.frozen?
+    def deep_freeze(arg)
+      case arg
+      when Hash
+        arg.each do |k, v|
+          deep_freeze(k)
+          deep_freeze(v)
         end
+        arg.freeze
+      when Enumerable
+        arg.each{|v| deep_freeze(v)}
+        arg.freeze
+      else
+        arg.freeze unless arg.frozen?
       end
     end
   end
