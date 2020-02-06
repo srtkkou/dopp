6048310c2912565f4a4ee5c009cece363acca18c
srtkkou <srtkkou@outlook.jp>
2020-02-06 13:36:21 +0900

    [Fix] Fixed const/error file to reduce rubocop messages.

--- lib/dopp/const.rb
+++ lib/dopp/const.rb
@@ -1,18 +1,12 @@
 # frozen_string_literal: true
 
-# File to define module constants.
-# 定数定義用ファイル.
 module Dopp
-  # Version of the application.
-  # アプリケーションのバージョン.
-  VERSION ||= '0.1.0'.freeze
+  # Application version.
+  VERSION ||= '0.1.0'
 
   # Application name.
-  # アプリケーション名.
   APPLICATION ||= self.name.downcase.freeze
 
   # Line feed code.
-  # 改行コード.
-  LF ||= "\n".freeze
+  LF ||= "\n"
 end
-
--- lib/dopp/error.rb
+++ lib/dopp/error.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Dopp
+  # Application error.
   class Error < StandardError
     class << self
       # Check value using "is_a?" with expected class.
@@ -8,29 +9,29 @@ module Dopp
       # @param [Class] expected Expected class.
       def check_is_a!(value, expected)
         return if value.is_a?(expected)
-        # Raise error.
-        raise(self.new("#{value}(#{value.class.name}) " +
-          "using 'is_a?' with #{expected.name} should be true."))
+
+        raise self, "#{value}'s class #{value.class.name} "\
+          "should be 'is_a?' with #{expected.name}."
       end
 
       # Check value using ">" with expected value.
       # @param [Object] value Value.
       # @param [Class] expected Expected value.
       def check_gt!(value, expected)
-        return if (value > expected)
-        # Raise error.
-        raise(self.new("#{value} should be " +
-          "greater than #{expected}."))
+        return if value > expected
+
+        raise self, "#{value} should be "\
+          "greater than #{expected}."
       end
 
       # Check value using ">=" with expected value.
       # @param [Object] value Value.
       # @param [Class] expected Expected value.
       def check_gteq!(value, expected)
-        return if (value >= expected)
-        # Raise error.
-        raise(self.new("#{value} should be " +
-          "greater than or equal to #{expected}."))
+        return if value >= expected
+
+        raise self, "#{value} should be "\
+          "greater than or equal to #{expected}."
       end
     end
   end
