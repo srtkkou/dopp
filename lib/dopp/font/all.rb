bc2a384399465c5c89de7f16a1d6ab9e0762f4bc
srtkkou <srtkkou@outlook.jp>
2020-02-10 14:12:10 +0900

    [Add] Added all/en/ja font file to include multiple fonts.
    * Added all/en/ja font file to include multiple fonts.
     (複数フォントを一気に追加するためのファイルを追加).

---
+++ lib/dopp/font/all.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+require 'dopp/font/en'
+require 'dopp/font/ja'
---
+++ lib/dopp/font/ja.rb
@@ -0,0 +1,4 @@
+# frozen_string_liteal: true
+
+require 'dopp/font/kozuka_mincho_pr6n_r'
+require 'dopp/font/kozuka_gothic_pr6n_m'
