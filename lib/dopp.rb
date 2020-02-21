be48b6ec4f787d41a525c6b260eb8081f1f071e4
srtkkou <srtkkou@outlook.jp>
2020-02-21 14:45:15 +0900

    [Update] Added require statement in dopp.rb which is (mostly) common for all sources.  (共通で使われるソースについてrequire節をdopp.rbに追加).

--- lib/dopp.rb
+++ lib/dopp.rb
@@ -1,17 +1,12 @@
 # frozen_string_literal: true
 
-# Global constants.
-# require 'dopp/const'
-
-# Utility functions.
-# require 'dopp/util'
-
-# PDF types.
-# require 'dopp/type'
+require 'dopp/const'
+require 'dopp/error'
+require 'dopp/font'
+require 'dopp/util'
 
 require 'dopp/document'
 
 # Top level module for gem Dopp.
 module Dopp
-  # Your code goes here...
 end
