f2f98d90f14e7e04951fa79e52b85792318c3c50
srtkkou <srtkkou@outlook.jp>
2020-02-21 14:28:41 +0900

    [Update] Moved application constants under Dopp module.  (アプリ共通の定数をDoppモジュール配下に移動).

--- lib/dopp/const.rb
+++ lib/dopp/const.rb
@@ -6,8 +6,23 @@ module Dopp
   VERSION ||= '0.1.0'
 
   # Application name.
-  APPLICATION ||= name.downcase.freeze
+  APP_NAME ||= name.downcase.freeze
 
   # Line feed code.
   LF ||= "\n"
+
+  # Default PDF version.
+  DEFAULT_PDF_VERSION ||= '1.4'
+
+  # Available page sizes.
+  # (Width * height in millimeters.)
+  PAGE_SIZES ||= {
+    A1: [594.0, 841.0], A2: [420.0, 584.0],
+    A3: [297.0, 420.0], A4: [210.0, 297.0],
+    A5: [148.0, 210.0], A6: [105.0, 148.0],
+    B1: [728.0, 1030.0], B2: [515.0, 728.0],
+    B3: [364.0, 515.0], B4: [257.0, 364.0],
+    B5: [182.0, 257.0], B6: [128.0, 182.0],
+    Letter: [215.9, 279.4]
+  }.freeze.each_value { |v| v.freeze }
 end
--- test/const_test.rb
+++ test/const_test.rb
@@ -5,21 +5,36 @@ require 'test_helper'
 module Dopp
   class ConstTest < Minitest::Test
     def test_ok_version
-      refute_nil(VERSION)
-      assert_includes(::Dopp.constants, :VERSION)
+      refute_nil(::Dopp::VERSION)
       assert_equal(true, VERSION.frozen?)
     end
 
     def test_ok_application
-      assert_equal('dopp', APPLICATION)
-      assert_includes(::Dopp.constants, :APPLICATION)
-      assert_equal(true, APPLICATION.frozen?)
+      assert_equal('dopp', ::Dopp::APP_NAME)
+      assert_equal(true, APP_NAME.frozen?)
     end
 
     def test_ok_lf
-      assert_equal("\n", LF)
-      assert_includes(::Dopp.constants, :LF)
+      assert_equal("\n", ::Dopp::LF)
       assert_equal(true, LF.frozen?)
+    end
+
+    def test_ok_default_pdf_version
+      assert_equal("1.4", ::Dopp::DEFAULT_PDF_VERSION)
+      assert_equal(true, DEFAULT_PDF_VERSION.frozen?)
+    end
+
+    def test_ok_page_sizes
+      PAGE_SIZES.each_value do |pair|
+        assert_instance_of(Array, pair)
+        assert_equal(2, pair.size)
+        assert_equal(true, pair.frozen?)
+        pair.each do |value|
+          assert_kind_of(Numeric, value)
+          assert_operator(value, :>, 0.0)
+        end
+      end
+      assert_equal(true, PAGE_SIZES.frozen?)
     end
   end
 end
