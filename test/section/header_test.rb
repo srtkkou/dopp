b4c11226baaf845a82e44f79d3f2efbf70f75925
srtkkou <srtkkou@outlook.jp>
2020-02-21 14:44:00 +0900

    [TODO] Temporary remove header test.

--- test/section/header_test.rb
+++ test/section/header_test.rb
@@ -6,11 +6,12 @@ require 'dopp/section/header'
 module Dopp
   module Section
     class HeaderTest < Minitest::Test
-      def test_ok_render
-        tail = [0xE2, 0xE3, 0xCF, 0xD3].pack('c*')
-        assert('%PDF-1.3%' + tail, Header.new.to_s)
-        assert('%PDF-1.4%' + tail, Header.new('1.4').to_s)
-      end
+      # TODO: Write this test again.
+      # def test_ok_render
+      #   tail = [0xE2, 0xE3, 0xCF, 0xD3].pack('c*')
+      #   assert('%PDF-1.3%' + tail, Header.new.to_s)
+      #   assert('%PDF-1.4%' + tail, Header.new('1.4').to_s)
+      # end
     end
   end
 end
